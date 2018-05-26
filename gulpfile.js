f3212dd7cbfb1b0da7f1b4cde6477336def2bbf5
Ben Furfie <benfurfie@bens-mac-mini.home>
2018-05-26 08:01:51 +0100

    Update gulpfile
    – Added new requires
    – Added notification of compilation to css:compile task
    – Added minify and css tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@
 var gulp = require('gulp'),
     postcss = require('gulp-postcss'),
     rename = require('gulp-rename'),
+    cleanCSS = require('gulp-clean-css'),
+    notify = require('gulp-notify'),
     tailwindcss = require('tailwindcss');
 
 /**
@@ -30,5 +32,39 @@ gulp.task('css:compile', function() {
         .pipe(rename({
             extname: '.css'
         }))
-        .pipe(gulp.dest('css/'));
+        .pipe(gulp.dest('css/'))
+        .pipe(notify(
+            {
+                message: 'Tailwind compiled'
+            }
+        ));
 });
+
+/**
+ * Minify the CSS
+ * 
+ * @since 1.0.0
+ */
+gulp.task('css:minify', ['css:compile'], function() {
+    return gulp.src([
+        './css/*.css',
+        '!./css/*.min.css'
+    ])
+    .pipe(cleanCSS())
+    .pipe(rename({
+        suffix: '.min'
+    }))
+    .pipe(gulp.dest('./css'))
+    .pipe(notify(
+        {
+            message: 'CSS minified'
+        }
+    ));
+});
+
+/**
+ * Run all CSS tasks
+ * 
+ * @since 1.0.0
+ */
+gulp.task('css', ['css:minify']);
